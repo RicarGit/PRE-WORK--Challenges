04ed9f5ec470977d968384fdfa7da26915340250
Ricardo Gava <Ricardoagava@gmail.com>
2021-08-18 01:44:36 -0300

    exercise 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const link = document.querySelector('[data-js="link"]')
+const app = document.querySelector('[data-js="app"]')
+app.innerHTML = `
   <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>`
+
+link.addEventListener('click', () => {
+  app.classList.toggle('hidden')
+  const isHidden = app.classList.contains('hidden')
+
+  link.textContent = isHidden ? 'Click on me to show!' : 'Click on me to hide!'
+})
