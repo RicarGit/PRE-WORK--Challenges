44140e8ff2f2704b016a3d9de8b409880d9d7c51
Ricardo Gava <Ricardoagava@gmail.com>
2021-08-19 05:29:56 -0300

    Added a commentary describing my mistake forgetting to concatenate both hobbies

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -9,7 +9,7 @@ const jane = {
   ...john,
   name: 'Jane',
   hobbies: ['MuayThai', 'Programming'] // in this part the Jane's hobbies should be concatenated with Jhon's hobbies
-}                                      // the code should be this: hobbies: [...jhon, 'MuayThai', 'Programming'], my mistake...
+}                                      // the code should be this: hobbies: [...jhon.hobbies, 'MuayThai', 'Programming'], my mistake...
 
 console.log('John:', john)
 console.log('Jane:', jane)
