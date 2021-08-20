7dd1b226fe3062863856a789b8ff78ad5f8cd704
Ricardo Gava <Ricardoagava@gmail.com>
2021-08-20 03:43:08 -0300

    Day 3 exercise 2

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,3 +1,5 @@
+//Exercise 1
+
 const form = document.querySelector('form')
 const p = document.createElement('p')
 
@@ -35,3 +37,54 @@ const showFormattedName = event => {
 }
 
 form.addEventListener('input', showFormattedName)
+
+
+
+//Exercise 2
+
+const select = document.createElement('select')
+form.insertAdjacentElement('beforeend', select)
+select.setAttribute('multiple', '')
+
+const colorOptions = {
+  red: '#ff0000',
+  green: '#00ff00',
+  blue: '#0000ff',
+  black: '#000000',
+  yellow: '#ffff00',
+  fuchsia: '#ff00ff',
+  navy: '#000080',
+  teal: '#008080'
+}
+
+const generateColorOptions = options => {
+  for (const color in options) {
+    const option = document.createElement('option')
+
+    select.appendChild(option)
+    option.setAttribute('value', options[color])
+    option.textContent = color
+  }
+}
+
+const clearDivElements = () => {
+  const divElements = document.querySelectorAll('div')
+  divElements.forEach(div => div.remove())
+}
+
+const generateColoredDivBySelectedColor = () => {
+  const selectChildrens = Array.from(select.children)
+  clearDivElements()
+
+  selectChildrens.forEach(option => {
+    if (option.selected) {
+      const div = document.createElement('div')
+
+      div.style.backgroundColor = option.value
+      form.insertAdjacentElement('afterend', div)
+    }
+  })
+}
+
+generateColorOptions(colorOptions)
+select.addEventListener('click', generateColoredDivBySelectedColor)
